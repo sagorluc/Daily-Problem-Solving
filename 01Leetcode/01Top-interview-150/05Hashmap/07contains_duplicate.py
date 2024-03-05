1ea1d4bb00bed2337c52198ca114bdb2589204a0
sagorluc <mdsagorluc@gmail.com>
2024-03-05 21:31:29 +0600

    contains duplicate II

---
+++ 01Leetcode/01Top-interview-150/05Hashmap/07contains_duplicate.py
@@ -0,0 +1,23 @@
+class Solution:
+    def containsNearbyDuplicate(self, nums: list[int], k: int) -> bool:
+        window = set()
+        left = 0
+        
+        for right in range(len(nums)):
+            # base case
+            if right - left > k:
+                window.remove(nums[left])
+                left += 1
+                
+            if nums[right] in window:
+                return True
+            else:
+                window.add(nums[right])
+
+        return False
+    
+input_arr = list(map(int, input('Enter an array: ').split()))
+target_value = int(input('Enter an target value: '))
+sl = Solution()
+res = sl.containsNearbyDuplicate(input_arr, target_value)
+print('Is contains near by duplicate: ', res)        
